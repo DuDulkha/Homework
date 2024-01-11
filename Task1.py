cb3036a3e3ef53035ac3b6a11c12eecaa1398494
DuDulkha <dudulkha@gmail.com>
2024-01-11 02:10:40 -0500

    commit 3

--- Task1.py
+++ Task1.py
@@ -2,9 +2,16 @@
 print("Hello Py4Econ!")
 
 # Question 1
-x = input("Enter a number: ")
+x = input("Enter your age: ")
 print("You entered: " + x + '. Done!')
 
 # Question 2
-y= input("Enter your name: ")
-print("User name:", y)
+y = input("Enter user name: ")
+#print("User name:", y)
+
+# Question 3
+z = input("Enter your password: ")
+if y == "Dudu" and z == "123456":
+    print("Welcome Mr Dudu")
+else:
+    print("Your user name or password is incorrect.")

2689cbaf3195996d7fe9018a6dd75be719b86039
DuDulkha <dudulkha@gmail.com>
2024-01-19 23:24:20 -0500

    Changed file name

---
+++ Homework_2.py
@@ -0,0 +1,21 @@
+# Start
+print("Hello Py4Econ!")
+
+# Question 1
+x = input("Enter your age: ")
+print("You entered: " + x + '. Done!')
+
+# Question 2
+y = input("Enter user name: ")
+#print("User name:", y)
+
+# Question 3
+z = input("Enter your password: ")
+if y == "Dudu" and z == "123456":
+    print("Welcome Mr. Dudu")
+else:
+    print("Your user name or password is incorrect.")
+
+# Question 4
+m = input("Enter your gender: ")
+print("Your gender:", m)

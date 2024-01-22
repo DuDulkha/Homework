69a04d577e4a5af14d52344930894403cdb185db
DuDulkha <dudulkha@gmail.com>
2024-01-21 19:40:50 -0500

    imported packages and data

---
+++ Homework_3.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import os
+os.getcwd()
+os.chdir(r'C:\Users\dudul\OneDrive\Documents\Dudu\Programming\Py4Econ\Introduction_Python\3_Dataframe\data')
+df = pd.read_excel("data.xlsx") # //
+
+######## Task 1 #########
+
+######## Task 2 #########

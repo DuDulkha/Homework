b9f3ec34a364d4c895a7f8dd35430f78c468f1a7
DuDulkha <dudulkha@gmail.com>
2024-07-13 22:30:01 -0400

    Task 1 Q1 updated

--- Homework_3.py
+++ Homework_3.py
@@ -6,32 +6,38 @@ import datetime
 import string
 import os
 os.getcwd()
-os.chdir(r'C:\Users\dudul\OneDrive\Documents\Dudu\Programming\Py4Econ\Introduction_Python\3_Dataframe\data')
+os.chdir(r'D:\\Programming\\Py4Econ\Introduction_Python\3_Dataframe\data')
 df = pd.read_excel("data.xlsx") # //
 
 ######## Task 1 #########
 ## Question 1:
 num_rows=50-len(df['id'])
 new_rows_data = []
+max_id = df['id'].max()
+
 for i in range(num_rows):
+    max_id += 1
     new_row = [
-        df['id'].max()+=1,                                                                                                                  # Column 1
-        str(df['firstName'].sample().values[0]),                                                                                            # Column 2            
-        str(df['lastName'].sample().values[0]),                                                                                             # Column 3
-        f"{''.join(random.choices(string.ascii_uppercase, k=2))}{random.randint(0, 99):02d}",                                               # Column 4
-        random.randint(18, 60),                                                                                                             # Column 5
-        random.randint(1, 40),                                                                                                              # Column 6
-        round(random.normalvariate(df['salary'].mean(), df['salary'].std()),1),                                                             # Column 7    
-        random.choice(["F", "M"]),                                                                                                          # Column 8
-        datetime.date.fromtimestamp( \                                                                                                      
-                random.uniform(datetime.datetime.timestamp(datetime.datetime.now()), \
-                               datetime.datetime.timestamp(datetime.datetime(1990, 1, 1, 0, 0, 0)))).strftime("%Y-%m-%d %H:%M:%S"),         # Column 9 
-        random.choice(["left", "right"]),                                                                                                   # Column 10    
-        ]
+        max_id,                                                                                     # Column 1
+        str(df['firstName'].sample().values[0]),                                                    # Column 2
+        str(df['lastName'].sample().values[0]),                                                     # Column 3
+        f"{''.join(random.choices(string.ascii_uppercase, k=2))}{random.randint(0, 99):02d}",       # Column 4
+        random.randint(18, 60),                                                                     # Column 5
+        random.randint(1, 40),                                                                      # Column 6
+        round(random.normalvariate(df['salary'].mean(), df['salary'].std()), 1),                    # Column 7
+        random.choice(["F", "M"]),                                                                  # Column 8
+        datetime.date.fromtimestamp(
+            random.uniform(
+                datetime.datetime.timestamp(datetime.datetime(1990, 1, 1, 0, 0, 0)),
+                datetime.datetime.timestamp(datetime.datetime.now())
+            )
+        ).strftime("%Y-%m-%d %H:%M:%S"),                                                            # Column 9
+        random.choice(["left", "right"]),                                                           # Column 10
+    ]
     new_rows_data.append(new_row)
-    new_rows_df = pd.DataFrame(new_rows_data, columns=df.columns)
+new_rows_df = pd.DataFrame(new_rows_data, columns=df.columns)
 df = pd.concat([df, new_rows_df], ignore_index=True)
-df
+print(df)
 
 ## Question 2:
 df1 = df[(df["age"]>27) & (df["gender"] != "M")]
